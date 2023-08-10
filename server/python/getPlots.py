0459412291ee8f01d6b7f11e5e1ce219d042ab29
Anshh Chaturvedi <72307339+Anshh1010@users.noreply.github.com>
2023-08-11 04:33:25 +0530

    Update getPlots.py

--- server/python/getPlots.py
+++ server/python/getPlots.py
@@ -10,18 +10,26 @@ def plot_stock_data(ax, data, title):
     ax.set_ylabel('Price')
 
 if __name__ == "__main__":
+    print("Helloo")
     if len(sys.argv) != 2:
         print("Usage: python plot_stock_data.py <stock_name>")
         sys.exit(1)
 
     stock_name = sys.argv[1]
-    periods = ['5y', '3y', '1y', '6mo', '3mo', '1mo']
+    periods = ['1y', '6mo', '3mo', '1mo']
     os.makedirs('plots', exist_ok=True)
-    for period in periods:
-        fig, ax = plt.subplots(figsize=(8, 6))
-        data = pd.read_csv(f"{stock_name}_{period}.csv")  # Replace with the actual CSV filename
+
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))  # Create a 2x2 grid of subplots
+    plt.subplots_adjust(hspace=0.5)
+
+    for i, period in enumerate(periods):
+        row = i // 2
+        col = i % 2
+        ax = axs[row, col]
+        data = pd.read_csv(f"{stock_name}.csv")  # Replace with the actual CSV filename
         plot_stock_data(ax, data, f'{stock_name} {period}')
         ax.set_facecolor('#2E2E2E')  # Set dark background
-        plt.tight_layout()
-        plt.savefig(os.path.join('plots', f'plot_{period}.png'))
-        plt.close()  # Close the plot to release resources
+
+    plt.tight_layout()
+    plt.savefig(os.path.join('plots', 'combined_plot.png'))  # Save the combined plot
+    plt.close()  # Close the plot to release resources
